dd130b6f02b6974f1bd5806d73e64593b9e46092
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-13 14:14:49 +0000

    Learned about how to access and insert an item in a list

--- 04 - Lists/02_access_and_insert_items.py
+++ 04 - Lists/02_access_and_insert_items.py
@@ -1,17 +1,21 @@
+print('--------------Access Items----------------')
 fruits = ['apple', 'mango', 'banana', 'orange']
 print(fruits) 
 
-
 # Access items
 print(fruits[1]) # mango
+print(fruits[2]) # banana
+print(fruits[0]) # apple
 
 # Change list
 fruits[1] = 'grapes'
 print(fruits) # ['apple', 'grapes', 'banana', 'apple', 'watermelon']
 
+
 # Insert item in a list
-
+print(' ')
+print('--------------Insert Items----------------')
 # To insert a new list item, without replacing any of the existing values, we can use insert() method. it inserts an item at specific index
-
+print('Before Insertion::', fruits)
 fruits.insert(2, 'oranges')
-print(fruits)
+print('After Insertion::,', fruits)
