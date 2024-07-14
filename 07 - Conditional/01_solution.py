7d61fa507897a4c01432d56ec940d7860e305caf
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-14 15:17:01 +0000

    Solved problem of Classify a person's age group: Child (< 13), Teenager (13-19), Adult (20-59), Senior (60+).

---
+++ 07 - Conditional/01_solution.py
@@ -0,0 +1,16 @@
+# 1. Age Group Categorization
+
+# Classify a person's age group: Child (< 13), Teenager (13-19), Adult (20-59), Senior (60+).
+
+age = input('Enter Your Age::')
+age = int(age)
+
+if age < 13:
+    print('Child')
+elif age < 20:
+    print('Teenager')
+elif age < 60:
+    print('Adult')
+else:
+    print('Senior')
+
