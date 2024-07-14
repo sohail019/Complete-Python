4a4049cd42bcd2c75d4db9c37e843b585f283ed4
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-14 09:48:15 +0000

    Learned about how to copy a dictionary with two methods: copy() and dict() constructor

---
+++ 05 - Dictionary/05_copy_a_dictionary.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------
+# Copy a Dictionary
+print('')
+print('--------------Copy a Dictionary----------------')
+
+# by using built in copy() method
+my_favt_copy = my_favt.copy()
+print(my_favt_copy)
+
+# by using dict() constructor
+
+my_favt_copy_2 = dict(my_favt_copy)
+print(my_favt_copy)
