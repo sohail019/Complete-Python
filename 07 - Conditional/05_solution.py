fca58fc8330e66852a0b5315fed724cdc71bb617
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-14 16:17:40 +0000

    Solved problem of Weather Activity Suggestion

---
+++ 07 - Conditional/05_solution.py
@@ -0,0 +1,13 @@
+# 5. Weather Activity Suggestion
+# Problem: Suggest an activity based on the weather (e.g., Sunny - Go for a walk, Rainy - Read a book, Snowy - Build a snowman).
+
+weather = input("Enter Today's Weather::")
+
+if weather == 'Sunny':
+  print('Go for a Walk')
+elif weather == 'Rainy':
+  print('Read a Book')
+elif weather == 'Snowy':
+  print('Build a snowman')
+else:
+  print('Bhai tu soja!')
