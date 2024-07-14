edd8a91afda7a2c74de960bf58301ed9cd9e4b34
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-14 08:39:46 +0000

    Learned about how to directly calculate square and cube in list in one line of code

---
+++ 04 - Lists/07_square_and_cube.py
@@ -0,0 +1,9 @@
+print('')
+print('----------------------Square and Cube in list ------------------------')
+# Square in List
+squared_nums = [x**2 for x in range(10)]
+print(squared_nums)
+
+# Cube in List
+cube_nums = [y**3 for y in range(20)]
+print(cube_nums)
