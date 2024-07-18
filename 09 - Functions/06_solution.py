b462c9a55fd39ed1de5883cbdcb7fc5422e80c93
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-18 04:58:20 +0000

    Learned about lambda function for one hand complex calculation

---
+++ 09 - Functions/06_solution.py
@@ -0,0 +1,12 @@
+# 6. Lambda Function
+# Problem: Create a lambda function to compute the cube of a number.
+
+# def cube(num):
+#     return num ** 3
+
+# print(cube(3))
+
+
+cube = lambda num: num ** 3
+
+print('Cube is', cube(3))
